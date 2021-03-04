a9837965e548d82b77e93e2fb1a917642c4cd6bd
Datsenko Bogdan <dacenkoboda1@gmail.com>
2021-03-04 16:29:13 +0200

    Add main function "CalcCSV"

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task1
 {
@@ -29,6 +27,83 @@ namespace Task1
             }
 
             return temp;
+        }
+
+        static void CalcCSV(string srcFile, string destFile)
+        {
+            StreamReader sr = new StreamReader(srcFile);
+            StreamWriter sw = new StreamWriter(destFile, true, System.Text.Encoding.Default);
+            string[] buff = new string[File.ReadAllLines(srcFile).Length];
+            string used = "";
+            string date = "";
+            int hoursCount = 0;
+            int count = 0;
+
+            for (int i = 0; !sr.EndOfStream; i++)
+            {
+                buff[i] = sr.ReadLine();
+            }
+
+            sr.BaseStream.Position = 1;
+
+            sw.Write("Name,Date");
+
+            for (int i = 1; i < buff.Length; i++)
+            {
+                if (!date.Contains(buff[i].Substring(buff[i].IndexOf(',') + 1, 11)))
+                {
+                    sw.Write("," + TransformDate(buff[i].Substring(buff[i].IndexOf(',') + 1, 11)));
+                    Console.WriteLine("," + TransformDate(buff[i].Substring(buff[i].IndexOf(',') + 1, 11)));
+                    date += buff[i];
+                }
+            }
+
+            while (!sr.EndOfStream)
+            {
+                string emploee = sr.ReadLine();
+                if (!used.Contains(emploee.Substring(0, emploee.IndexOf(','))))
+                {
+                    for (int i = 0; i < buff.Length; i++)
+                    {
+                        if (buff[i].Substring(0, buff[i].IndexOf(',')) == emploee.Substring(0, emploee.IndexOf(',')))
+                        {
+
+                            if (hoursCount > 0)
+                            {
+                                hoursCount--;
+                            }
+
+                            if (count == 0)
+                            {
+                                sw.Write(buff[i].Substring(0, buff[i].IndexOf(',')));
+                            }
+
+                            try
+                            {
+                                sw.Write("," + buff[i].Substring(buff[i].LastIndexOf(",") + 1, 3));
+                            }
+                            catch
+                            {
+                                sw.Write("," + buff[i].Substring(buff[i].LastIndexOf(",") + 1, 1));
+                            }
+                            count++;
+                        }
+                    }
+
+                    used += emploee.Substring(0, emploee.IndexOf(','));
+
+                    if (hoursCount > 0)
+                    {
+                        sw.Write(string.Concat(Enumerable.Repeat(",0", hoursCount)));
+                    }
+
+                    sw.Write("\n");
+                    hoursCount = 6;
+                    count = 0;
+                }
+
+            }
+            sw.Close();
         }
 
         static void Main(string[] args)
