0920b24ea334d78839b19e3510d451f504eb66b7
Datsenko Bogdan <dacenkoboda1@gmail.com>
2021-03-04 16:20:31 +0200

    Described logic in main

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string srcFile = "acme_worksheet.csv";
+            string destFile = "result.txt";
+
+            try
+            {
+                CalcCSV(srcFile, destFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Done...");
+            Console.ReadLine();
+        }
+    }
+}
