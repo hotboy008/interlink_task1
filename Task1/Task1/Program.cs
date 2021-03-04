3970efd8a7cf3f5c5a43e40a2ac134c4d86e7e2a
Datsenko Bogdan <dacenkoboda1@gmail.com>
2021-03-04 16:25:36 +0200

    Add function for transform date(TransformDate)

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -8,6 +8,29 @@ namespace Task1
 {
     class Program
     {
+        static string TransformDate(string date)
+        {
+            string temp = "";
+
+            switch (date.Substring(0, 3))
+            {
+                case "Jun":
+                    temp = date.Replace("Jun", "06");
+                    temp = temp.Replace(" ", "-");
+                    break;
+
+                case "Jul":
+                    temp = date.Replace("Jul", "07");
+                    temp = temp.Replace(" ", "-");
+                    break;
+
+                default:
+                    break;
+            }
+
+            return temp;
+        }
+
         static void Main(string[] args)
         {
             string srcFile = "acme_worksheet.csv";
